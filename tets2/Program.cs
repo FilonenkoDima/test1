9977fdc60a1f56b1e235605e0a49d13d4020b272
FilonenkoDima <filonenkodmytrodm@gmail.com>
2022-05-25 16:51:00 +0300

    edit

--- tets2/Program.cs
+++ tets2/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string str1 = "https://github.com/FilonenkoDima/test1.git";
+string str2 = "https://github.com/FilonenkoDima/test1.git";
+Console.WriteLine(str1 == str2);
