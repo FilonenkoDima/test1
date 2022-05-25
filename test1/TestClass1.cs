0d6f8be05ddc015f4caf417f746e8972fb3da07b
FilonenkoDima <filonenkodmytrodm@gmail.com>
2022-05-25 16:49:31 +0300

    Добавьте файлы проекта.

---
+++ test1/TestClass1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class TestClass1
+    {
+        public void Foo()
+        {
+
+        }
+
+        public void Bar()
+        {
+
+        }
+    }
+}
---
+++ tets2/TestClass2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tets2
+{
+    internal class TestClass2
+    {
+        public void Foo()
+        {
+
+        }
+
+        public void Bar()
+        {
+
+        }
+    }
+}
